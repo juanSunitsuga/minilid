11349dece80d93354b65fc5c0e5413816cf2833c
Agustinus Juan Kurniawan <juankurniawan04@gmail.com>
2025-05-10 21:22:51 +0700

    Edited MUI

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0a66c2', // LinkedIn blue
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 )
